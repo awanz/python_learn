117c97858823f87f1e8fd567bbb365266f0a1802
Awan <hermawan.musamauza@ai.astra.co.id>
2022-08-15 08:56:17 +0700

    update string

---
+++ 007_string.py
@@ -0,0 +1,25 @@
+print("Awan")
+
+nama = "Hermawan"
+print(nama)
+
+arrayString = "Hello, World!"
+print(arrayString)
+
+print(len(arrayString))
+
+print("Hel" in arrayString)
+print("Aw" in arrayString)
+print("Aw" not in arrayString)
+
+b = "Hermawan"
+print(b[2:5])
+print(b[:5])
+print(b[2:])
+print(b[:-5])
+print(b.upper())
+print(b.lower())
+namaLengkap = "Hermawan Musamauza "
+print(namaLengkap.strip())
+print(namaLengkap.replace(" ", "-"))
+print(namaLengkap.split(" "))
