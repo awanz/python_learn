a17cfdc84dea040e26f668416e56d1a7e93897db
Awan <hermawan.musamauza@ai.astra.co.id>
2022-08-20 23:04:02 +0700

    list

---
+++ 009_list.py
@@ -0,0 +1,11 @@
+myList = ["red", "blue", "yellow"]
+print(myList[1])
+print(len(myList))
+print(type(myList))
+
+youList = list(("one", "two", "three"))
+print(youList)
+youList.append("four")
+print(youList)
+youList.insert(2, "tes")
+print(youList)
