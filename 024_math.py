4e38fc538b1ff1b19336d74554c892d5ab04d417
awan <me.awan.es@gmail.com>
2022-11-15 05:49:39 +0700

    math and math advance

---
+++ 024_math.py
@@ -0,0 +1,8 @@
+findMinNumber = min(4,5,2,9,8)
+print(findMinNumber)
+
+findMaxNumber = max(6,4,2,7,4,9,1)
+print(findMaxNumber)
+
+positifNumber = abs(-4352)
+print(positifNumber)
---
+++ 024_math_advance.py
@@ -0,0 +1,12 @@
+import math
+
+x = math.sqrt(25)
+print(x)
+
+y = math.ceil(5.1)
+print(y) #pembulatan keatas
+z = math.floor(5.8)
+print(z) #pembulatan kebawah
+
+phi = math.pi
+print(phi)
